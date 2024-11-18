55fd35058314ae6850eb04d66af3196f3d05d5a9
Takeso-user <75063664+Takeso-user@users.noreply.github.com>
2024-11-18 08:28:02 +0100

    tests updated

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -2,52 +2,62 @@ package cache
 
 import (
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSet(t *testing.T) {
-	c := NewCache()
+	c := NewCache(time.Second * 10)
 	key := "userId"
 	value := 42
 
 	c.Set(key, value)
 
-	if val, found := c.Get(key); !found || val != value {
-		t.Errorf("Set() failed: expected %v, got %v", value, val)
-	}
+	val, found := c.Get(key)
+	assert.True(t, found, "expected key to be found")
+	assert.Equal(t, value, val, "expected value to be equal")
+}
+
+func TestCache_Expiration(t *testing.T) {
+	cache := NewCache(1 * time.Second)
+	cache.Set("key1", "value1")
+	time.Sleep(2 * time.Second)
+
+	_, ok := cache.Get("key1")
+	assert.False(t, ok, "expected key1 to be expired")
 }
 
 func TestGet(t *testing.T) {
-	c := NewCache()
+	c := NewCache(time.Second * 10)
 	key := "userId"
 	value := 42
 
 	c.Set(key, value)
 
-	if val, found := c.Get(key); !found || val != value {
-		t.Errorf("Get() failed: expected %v, got %v", value, val)
-	}
+	val, found := c.Get(key)
+	assert.True(t, found, "expected key to be found")
+	assert.Equal(t, value, val, "expected value to be equal")
 
-	if val, found := c.Get("nonExistingKey"); found || val != nil {
-		t.Errorf("Get() for non-existing key failed: expected nil, got %v", val)
-	}
+	val, found = c.Get("nonExistingKey")
+	assert.False(t, found, "expected key to not be found")
+	assert.Nil(t, val, "expected value to be nil")
 }
 
 func TestDelete(t *testing.T) {
-	c := NewCache()
+	c := NewCache(time.Second * 10)
 	key := "userId"
 	value := 42
 
 	c.Set(key, value)
 
-	if deleted := c.Delete(key); !deleted {
-		t.Errorf("Delete() failed: expected true for existing key")
-	}
+	deleted := c.Delete(key)
+	assert.True(t, deleted, "expected true for existing key")
 
-	if val, found := c.Get(key); found || val != nil {
-		t.Errorf("Delete() failed: expected key to be deleted, but got %v", val)
-	}
+	val, found := c.Get(key)
+	assert.False(t, found, "expected key to be deleted")
+	assert.Nil(t, val, "expected value to be nil")
 
-	if deleted := c.Delete("nonExistingKey"); deleted {
-		t.Errorf("Delete() failed: expected false for non-existing key")
-	}
+	deleted = c.Delete("nonExistingKey")
+	assert.False(t, deleted, "expected false for non-existing key")
 }
